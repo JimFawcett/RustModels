c0ef6b043354ef957d5f0f2c30aa234ebe22da54
James W. Fawcett <jfawcett@twcny.rr.com>
2020-03-28 13:27:27 -0400

    added code for RustModels presentation

--- WellDefinedBehavior/target/rls/debug/deps/well_defined_behavior-39ecca579c77fe74.d
+++ WellDefinedBehavior/target/rls/debug/deps/well_defined_behavior-39ecca579c77fe74.d
@@ -1,5 +1,5 @@
-c:\su\temp\WellDefinedBehavior\target\rls\debug\deps\well_defined_behavior-39ecca579c77fe74.rmeta: src\main.rs
+c:\github\JimFawcett\RustModels\WellDefinedBehavior\target\rls\debug\deps\well_defined_behavior-39ecca579c77fe74.rmeta: src\main.rs
 
-c:\su\temp\WellDefinedBehavior\target\rls\debug\deps\well_defined_behavior-39ecca579c77fe74.d: src\main.rs
+c:\github\JimFawcett\RustModels\WellDefinedBehavior\target\rls\debug\deps\well_defined_behavior-39ecca579c77fe74.d: src\main.rs
 
 src\main.rs:
--- WellDefinedBehavior/target/rls/debug/deps/well_defined_behavior-66f731bd77742575.d
+++ WellDefinedBehavior/target/rls/debug/deps/well_defined_behavior-66f731bd77742575.d
@@ -1,5 +1,5 @@
-c:\su\temp\WellDefinedBehavior\target\rls\debug\deps\well_defined_behavior-66f731bd77742575.rmeta: src\main.rs
+c:\github\JimFawcett\RustModels\WellDefinedBehavior\target\rls\debug\deps\well_defined_behavior-66f731bd77742575.rmeta: src\main.rs
 
-c:\su\temp\WellDefinedBehavior\target\rls\debug\deps\well_defined_behavior-66f731bd77742575.d: src\main.rs
+c:\github\JimFawcett\RustModels\WellDefinedBehavior\target\rls\debug\deps\well_defined_behavior-66f731bd77742575.d: src\main.rs
 
 src\main.rs:
