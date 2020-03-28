d70c1268e62438002c47915bf7afd1ab6f4eeaf5
James W. Fawcett <jfawcett@twcny.rr.com>
2020-03-27 21:50:15 -0400

    rebuilt

--- references/target/rls/debug/deps/references-ba9c117f26224d00.d
+++ references/target/rls/debug/deps/references-ba9c117f26224d00.d
@@ -1,5 +1,5 @@
-c:\su\temp\references\target\rls\debug\deps\references-ba9c117f26224d00.rmeta: src\main.rs
+c:\github\JimFawcett\RustModels\references\target\rls\debug\deps\references-ba9c117f26224d00.rmeta: src\main.rs
 
-c:\su\temp\references\target\rls\debug\deps\references-ba9c117f26224d00.d: src\main.rs
+c:\github\JimFawcett\RustModels\references\target\rls\debug\deps\references-ba9c117f26224d00.d: src\main.rs
 
 src\main.rs:
